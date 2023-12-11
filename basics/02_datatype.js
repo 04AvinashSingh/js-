b472fe2081b87aad2ea534d54edc5a42cb1ab075
04AvinashSingh <151105949+04AvinashSingh@users.noreply.github.com>
2023-12-11 19:51:10 +0000

    datatypes

---
+++ basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newer version
+// alart(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3)// code readability should be high
+
+console.log("Avinash")
+
+
+let name="Avinash"
+let age=18
+let isLoggedIn=false
+
+//numbee => 2 to power 53
+// bigint
+// string=> ""
+// boolean=> true/false
+//null=> standalone value
+// undefined=>
+// symbol=> unique
+
+
+//object
+
+console.log(typeof "Avinash")
+console.log(typeof null);//Object
+console.log(typeof undefined);// undefined
+
