fb9d3ea20a75cff3f2edcfd55476b631481b1a58
04AvinashSingh <151105949+04AvinashSingh@users.noreply.github.com>
2023-12-20 14:04:41 +0000

    Array

--- basics2/01_array.js
+++ basics2/01_array.js
@@ -28,18 +28,53 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // slice, splice
 
-console.log("A ", myArr);
+//console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)//will return the element from 1 to 3 but not change the original elelment
 //it will not include 3
-console.log(myn1);
-console.log("B ", myArr);
+//console.log(myn1);
+//console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3)//remove the the element from 1 to 3 return the removed array
 //it will change the array
-console.log("C ", myArr);
-console.log(myn2);
+//console.log("C ", myArr);
+//console.log(myn2);
+
+const marval_heros=["thor","Ironman","Siderman"]
+const dc=["superman","flash","batman"]
+
+//marval_heros.push(dc)//this will add dc into marval 
+
+const alheros =marval_heros.concat(dc)// this makes new array which has both the array
+//console.log(marval_heros);
+// console.log(alheros);
+
+const allnewhero=[...marval_heros,...dc]
+
+console.log(allnewhero);
+
+const anotherarray=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const realanotherarray=anotherarray.flat(Infinity)
+
+console.log(realanotherarray);
+
+console.log(Array.isArray("Avinash"));
+console.log(Array.from("Avinash"));
+console.log(Array.from({name:"hitesh"}));//interesting
+
+score1=100
+score2=200
+score3=300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
 
 
 
